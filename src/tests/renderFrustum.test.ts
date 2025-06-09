9e508522e1afcbf9872a08ab747b2d6857723fcc
suvendukungfu <ssuvendukumar489@gmail.com>
2025-06-09 16:54:51 +0530

    test(render): add view frustum culling verification suite for off-screen terrain meshes

--- src/tests/renderFrustum.test.ts
+++ src/tests/renderFrustum.test.ts
@@ -20,3 +20,6 @@ describe('Render Frustum & LOD Culling Suite', () => {
 
 // Senior Test: Assert bounding box culling efficiency for complex GLTF models
  // Commit Entry #5032
+
+// Senior Test: Assert bounding box culling efficiency for complex GLTF models
+ // Commit Entry #5063
