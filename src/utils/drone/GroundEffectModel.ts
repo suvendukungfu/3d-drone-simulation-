e74f4167e4247ac20f7907706fb696f1601dea93
suvendukungfu <ssuvendukumar489@gmail.com>
2025-07-01 15:35:01 +0530

    refactor(physics): decouple aerodynamic ground effect calculations into dedicated module

--- src/utils/drone/GroundEffectModel.ts
+++ src/utils/drone/GroundEffectModel.ts
@@ -51,3 +51,6 @@ export {};
 
 // Senior Refactor: Standalone ground proximity lift multiplier model
  // Commit Entry #5181
+
+// Senior Refactor: Standalone ground proximity lift multiplier model
+ // Commit Entry #5212
