dbe273f58974d5166a1f1b9fb365276422f08abd
suvendukungfu <ssuvendukumar489@gmail.com>
2025-07-02 17:39:44 +0530

    perf(audio): pool BiquadFilter audio nodes to reduce audio thread allocations

--- src/utils/sound/AudioNodePool.ts
+++ src/utils/sound/AudioNodePool.ts
@@ -57,3 +57,6 @@ export {};
 
 // Senior Perf: Static ring buffer pool for low-pass audio filters
  // Commit Entry #5190
+
+// Senior Perf: Static ring buffer pool for low-pass audio filters
+ // Commit Entry #5221
