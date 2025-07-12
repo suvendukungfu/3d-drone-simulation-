f10e607937ce0dd48ae56ea91a8e352b9c32759d
suvendukungfu <ssuvendukumar489@gmail.com>
2025-07-12 17:46:45 +0530

    refactor(bridge): decouple WebHID joystick input listener into standalone service

--- src/utils/controller/JoystickService.ts
+++ src/utils/controller/JoystickService.ts
@@ -63,3 +63,6 @@ export {};
 
 // Senior Refactor: Event-driven WebHID gamepad input mapping interface
  // Commit Entry #5261
+
+// Senior Refactor: Event-driven WebHID gamepad input mapping interface
+ // Commit Entry #5292
