b398a863e0328274f3e3ddf009021cb2b7254526
suvendukungfu <ssuvendukumar489@gmail.com>
2025-07-14 15:21:40 +0530

    feat(render): implement dynamic shadow map resolution scaling based on frame rate

--- src/components/EnvironmentManager.tsx
+++ src/components/EnvironmentManager.tsx
@@ -914,3 +914,6 @@ useGLTF.preload('/models/classroom.glb');
 
 // Senior Feature: Adaptive shadow map size (512px - 2048px) based on rolling FPS average
  // Commit Entry #5276
+
+// Senior Feature: Adaptive shadow map size (512px - 2048px) based on rolling FPS average
+ // Commit Entry #5307
