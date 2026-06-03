4abe93f2f01ff3a336df064bb59266e84f2b0bc1
suvendukungfu <ssuvendukumar489@gmail.com>
2026-06-03 12:00:00 +0530

    build: configure tailwindcss layout presets

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        pluto: {
+          dark: '#0B0F19',
+          light: '#E2E8F0',
+          accent: '#00F0FF',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
