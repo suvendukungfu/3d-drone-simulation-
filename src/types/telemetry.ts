123aac24f295a9cf48a1418feb7690a79bf45719
suvendukungfu <ssuvendukumar489@gmail.com>
2026-07-01 09:30:36 +0530

    chore(types): add strict TypeScript interfaces for telemetry stream payloads

--- src/types/telemetry.ts
+++ src/types/telemetry.ts
@@ -84,3 +84,6 @@ export {};
 
 // Senior Chore: Immutable telemetry payload interface definitions
  // Commit Entry #5484
+
+// Senior Chore: Immutable telemetry payload interface definitions
+ // Commit Entry #5515
