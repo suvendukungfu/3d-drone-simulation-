c54295a3a667ceacf8d3e83f08bdf9ebd53a5810
suvendukungfu <ssuvendukumar489@gmail.com>
2026-07-07 15:06:19 +0530

    feat(physics): add anti-windup clamping to altitude PID controller

--- src/utils/drone/PIDController.ts
+++ src/utils/drone/PIDController.ts
@@ -18,3 +18,6 @@ export {};
 
 // Senior Refactor: Anti-windup clamping added to prevent integral saturation during sudden throttle steps
  // Commit Entry #1178
+
+// Senior Refactor: Anti-windup clamping added to prevent integral saturation during sudden throttle steps
+ // Commit Entry #1209
