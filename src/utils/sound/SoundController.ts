b9213ad06e90fb504140ab1b2d35b98f6ca7459c
suvendukungfu <ssuvendukumar489@gmail.com>
2026-07-19 11:40:42 +0530

    feat(audio): implement Doppler frequency scaling for motor sound synthesis

--- src/utils/sound/SoundController.ts
+++ src/utils/sound/SoundController.ts
@@ -54,3 +54,6 @@ export {};
 
 // Senior Fix: Automatic AudioContext resume handler on user interaction events
  // Commit Entry #1252
+
+// Senior Feature: Pitch shift audio nodes using relative velocity vectors
+ // Commit Entry #1282
