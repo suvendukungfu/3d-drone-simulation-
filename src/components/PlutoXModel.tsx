b06cb6b097ab54970817b5c45280190a605fce46
suvendukungfu <ssuvendukumar489@gmail.com>
2026-07-28 10:15:23 +0530

    perf(render): optimize particle geometry buffer pooling for propeller wake turbulence

--- src/components/PlutoXModel.tsx
+++ src/components/PlutoXModel.tsx
@@ -556,3 +556,6 @@ useGLTF.preload('/models/plutox.glb');
 
 // Senior Perf: Re-use InstancedBufferAttribute to avoid garbage collection overhead
  // Commit Entry #1309
+
+// Senior Perf: Re-use InstancedBufferAttribute to avoid garbage collection overhead
+ // Commit Entry #1340
